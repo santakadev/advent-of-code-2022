1a15697dbc6e1ee540769b5c7c21e75426a5c3b7
Dani Santamaría <santaka87@gmail.com>
2022-12-05 18:04:15 +0100

    feat(day-04): 1st star with rust

---
+++ day-04/1st-⭐/rust/src/main.rs
@@ -0,0 +1,62 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct ElvesPair {
+    first: Elve,
+    second: Elve,
+}
+
+#[derive(Debug)]
+struct Elve {
+    min: u32,
+    max: u32,
+}
+
+impl FromStr for ElvesPair {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s.split(",");
+        let first = Elve::from_str(iter.next().unwrap()).unwrap();
+        let second = Elve::from_str(iter.next().unwrap()).unwrap();
+        Ok(ElvesPair { first, second })
+    }
+}
+
+impl Elve {
+    fn covers(&self, other: &Elve) -> bool {
+        self.min <= other.min && self.max >= other.max
+    }
+}
+
+impl ElvesPair {
+    fn covers(&self) -> bool {
+        self.first.covers(&self.second) || self.second.covers(&self.first)
+    }
+}
+
+impl FromStr for Elve {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s.split("-");
+        let min = u32::from_str(iter.next().unwrap()).unwrap();
+        let max = u32::from_str(iter.next().unwrap()).unwrap();
+        Ok(Elve { min, max })
+    }
+}
+
+fn main() {
+    let input = std::fs::read_to_string("src/input.txt").unwrap();
+
+    let solution = input
+        .trim_end()
+        .split("\n")
+        .map(|line| line .parse::<ElvesPair>() .unwrap())
+        .map(|pair| pair.covers())
+        .filter(|covers| *covers)
+        .count();
+
+    println!("{:?}", solution);
+}
+
